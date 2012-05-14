0d15186d17ccaa27244043ecdf7669a3e2a9cb3e
Michael <mars8082686@gmail.com>
2012-05-13 21:46:07 -0400

    added indexes

--- app/models/belief.rb
+++ app/models/belief.rb
@@ -1,6 +1,7 @@
 class Belief < Neo4j::Rails::Model
   index :title
   property :title, :type => String
+  validates :title, :uniqueness => true
   property :description, :type => String
   
   has_n(:users)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < Neo4j::Rails::Model
   index :name
+  
   property :name, :type => String
-
+  validates :name, :uniqueness => true
   
   has_n(:beliefs)
 
