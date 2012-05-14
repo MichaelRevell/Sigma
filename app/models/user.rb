bab42e733942ca6e4ab9422209058dca59350d20
Michael <mars8082686@gmail.com>
2012-05-13 20:03:45 -0400

    added users scaffold

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < Neo4j::Rails::Model
+  property :name, :type => String
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sigma::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
