7a3fc025253afef4e834fd4dcd3560a51c4087ec
Michael <mars8082686@gmail.com>
2012-05-13 20:24:53 -0400

    user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Sigma::Application.routes.draw do
-  get "sessions/new"
-
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  
   root :to => 'home#index'
 
   get "home/add_beliefs"
@@ -8,6 +10,7 @@ Sigma::Application.routes.draw do
   resources :beliefs
 
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
