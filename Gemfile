67a57b758f2d04ed99dbe88c9c8102fb1b5971b0
Dan Schlossberg <danschlossberg@gmail.com>
2012-05-22 00:39:03 -0400

    json

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'heroku'
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
--- app/helpers/beliefs_helper.rb
+++ app/helpers/beliefs_helper.rb
@@ -1,2 +1,9 @@
 module BeliefsHelper
+   def self.generate_beliefs (belief_count)
+    beliefs=Array.new { }
+    for i in 0..belief_count-1
+      beliefs[i] = "b#{i}"
+    end
+    return beliefs
+  end
 end
